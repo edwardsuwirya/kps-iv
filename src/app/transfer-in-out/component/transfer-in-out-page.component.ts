bc490a581a708895ae816d7034d923a01981f3d5
Edward Suwirya <edward.suwirya@btpn.com>
2017-05-13 08:35:16 +0700

    2nd init layout Transfer In/Out Module

--- src/app/transfer-in-out/component/transfer-in-out-page.component.ts
+++ src/app/transfer-in-out/component/transfer-in-out-page.component.ts
@@ -1,41 +1,44 @@
 /**
  * Created by edo on 10/05/2017.
  */
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, AfterViewInit} from "@angular/core";
 import {Observable} from "rxjs/Rx";
 import {TransferDetail} from "../model/transfer-detail";
 
 declare let $:any;
-declare let _:any;
 
 @Component({
     selector: 'app-transferinout-page',
     templateUrl: './transfer-in-out-page.component.html',
     styleUrls: ['./transfer-in-out-page.component.css']
 })
-export class TransferInOutPageComponent implements OnInit {
+export class TransferInOutPageComponent implements OnInit,AfterViewInit {
     detailInfos:TransferDetail[] = [new TransferDetail('', '', 0)];
 
     constructor() {
     }
 
-    ngOnInit():void {
+    ngOnInit() {
+
     }
 
-    onAddMoreDetail() {
-        this.detailInfos.push(new TransferDetail('', '', 0));
+    ngAfterViewInit() {
     }
 
-    onRemoveDetail(info:TransferDetail) {
+    onAddMoreDetail(idx) {
+        this.detailInfos.splice(idx + 1, 0, new TransferDetail('', '', 0, ''));
+    }
+
+    onRemoveDetail(idx) {
         if (this.detailInfos.length > 1) {
-            _.remove(this.detailInfos, {'article': info.article, 'qty': info.qty});
+            this.detailInfos.splice(idx, 1);
         }
     }
 
     onFocusEditor(editorId:string, labelId:string) {
         Observable.timer(100).do(()=> {
-            document.getElementById(editorId).removeAttribute('hidden');
-            document.getElementById(labelId).setAttribute('hidden', 'hidden');
+            document.getElementById(editorId).style.display = 'block';
+            document.getElementById(labelId).style.display = 'none';
             $('#' + editorId).focus();
         }).subscribe();
 
@@ -43,8 +46,8 @@ export class TransferInOutPageComponent implements OnInit {
 
     onLostFocusEditor(editorId:string, labelId:string) {
         Observable.timer(100).do(()=> {
-            document.getElementById(labelId).removeAttribute('hidden');
-            document.getElementById(editorId).setAttribute('hidden', 'hidden');
+            document.getElementById(labelId).style.display = 'block';
+            document.getElementById(editorId).style.display = 'none';
         }).subscribe();
     }
 }
--- src/app/transfer-in-out/model/transfer-detail.ts
+++ src/app/transfer-in-out/model/transfer-detail.ts
@@ -5,10 +5,12 @@ export class TransferDetail {
     article:string;
     serialNo:string;
     qty:number;
+    uom:string;
 
-    constructor(article?:string, serialNo?:string, qty?:number) {
+    constructor(article?:string, serialNo?:string, qty?:number, uom?:string) {
         this.article = article;
         this.serialNo = serialNo;
         this.qty = qty;
+        this.uom = uom;
     }
 }
